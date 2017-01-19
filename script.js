067144d264b2587ef6f7fdf8c673314d4bc57d45
Daniel Gardner <daniel.marc.gardner@gmail.com>
2017-01-18 17:02:06 -0800

    Created CheckerBoard and Random Colors

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// Your JS goes here
+
+// Checkerboard!!!
+for (let i = 1; i < 82; i++) {
+    if (i % 2 === 0) {
+      let red = document.createElement('div')
+      red.innerHTML = "<div style='display:block; background-color:#FF0000;padding-bottom:11.1%;float:left;width:11.1%'>&nbsp;</div>";
+      document.body.append(red);
+    } else {
+      let black = document.createElement('div');
+      black.innerHTML = "<div style='display:block; background-color:#000000;padding-bottom:11.1%;float:left;width:11.1%'>&nbsp;</div>";
+      document.body.append(black);
+    }
+}
+
+// //COLOR GENERATOR!!!!!
+function colorGenerator() {
+  let hexOp = "abcdef1234567890";
+  let hex = '#';
+  for (let i = 0; i < 6; i++) {
+    hex += hexOp[Math.floor(Math.random()*16)];
+  }
+  return hex;
+}
+
+for (let i = 1; i < 82; i++) {
+      let randomColor = colorGenerator();
+      let rand = document.createElement('div');
+      rand.innerHTML = "<div style='display:block; background-color:"+randomColor+";padding-bottom:11.1%;float:left;width:11.1%'>&nbsp;</div>";
+      document.body.append(rand);
+}
